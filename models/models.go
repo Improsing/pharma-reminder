e9825b3df560721d1d1f3fb3962f942779ce28f9
Improsing <bankurovmuslim@yandex.ru>
2025-03-07 22:40:30 +0300

    models

--- models/models.go
+++ models/models.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Schedule struct {
+	ID            int        `json:"id"`
+	UserID        string     `json:"user_id"`
+	MedicineName  string     `json:"medicine_name"`
+	Frequency     int        `json:"frequency"`
+	Duration      *int       `json:"duration,omitempty"`
+	StartTime     time.Time  `json:"start_time"`
+}
